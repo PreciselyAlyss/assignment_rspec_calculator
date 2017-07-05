057f9267c1425715faf3a60054c562e29b523f58
Alyss Noland <alyss.noland@gmail.com>
2017-07-04 23:14:18 -0500

    WIP: Calculator tests

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,70 @@
-# Your code here
+describe Calculator do
+
+  let(:calculator){ Calculator.new}
+
+  describe '#initialize' do
+    # stuff here
+  end
+
+  describe '#add' do
+    it 'raises an ArgumentError with 0 arguments' do
+      expect(calculator.add).to raise_error(ArgumentError)
+    end
+
+    it 'returns the sum of its arguments' do
+      expect(calculator.add(1, 2)).to eq(3)
+    end
+  end
+
+  describe '#subtract' do
+    it 'raises an ArgumentError with 0 arguments' do
+      expect(calculator.subtract).to raise_error(ArgumentError)
+    end
+
+    it 'returns the sum of its arguments' do
+      expect(calculator.subtract(2, 1)).to eq(1)
+    end
+  end
+
+  describe '#multiply' do
+    it 'returns the sum of its arguments' do
+      expect(calculator.multiply(2, 2)).to eq(4)
+    end
+  end
+
+  describe '#divide' do
+    it 'returns the sum of its arguments' do
+      expect(calculator.divide(4, 2)).to eq(2)
+    end
+  end
+
+  describe '#pow' do
+    it 'returns the sum of its arguments' do
+      expect(calculator.pow(3, 2)).to eq(9)
+    end
+  end
+
+  describe '#sqrt' do
+    it 'returns the sum of its argument' do
+      expect(calculator.sqrt(9)).to eq(3)
+    end
+  end
+
+  describe '#memory' do
+    # stuff here
+  end
+
+  describe '#memory=' do
+    it 'actually sets the variable' do
+      test_mem = 8
+
+      calculator.memory = test_mem
+      expect(calculator.memory).to eq(test_mem)
+    end
+  end
+
+  describe '#stringify' do
+    # stuff here
+  end
+
+end
